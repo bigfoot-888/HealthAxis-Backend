28e936fe7231f6ee6737e3b6670809141bc65f2a
bigfoot-888 <davidxuhu14@gmail.com>
2026-05-09 14:28:07 +0200

    fix: detalle auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 const ForbiddenError = require('../errors/ForbiddenError');
 function requireAuth(req, res, next) {
+    if (!req.user) 
         return next(new ForbiddenError('Acceso no autorizado.'));
     next();
 }
