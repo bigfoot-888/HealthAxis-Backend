4b211f021fbfc91d9fd39ed004a25e0e56156bc8
bigfoot-888 <davidxuhu14@gmail.com>
2026-05-20 14:21:43 +0200

    add: readme

--- utils/populate-db.js
+++ utils/populate-db.js
@@ -9,7 +9,6 @@ const {
     DiagnosisUser,
     Treatment,
     TreatmentUser,
-    DiagnosisTreatment,
     Role,
     UserDashboard,
     DashboardComponent,
@@ -18,6 +17,7 @@ const {
 
 const { hashPassword } = require('./password.utils');
 const { faker } = require('@faker-js/faker');
+const {DEFAULT_DASHBOARD_COMPONENTS} = require('../config/dashboard'); 
 
 // ===== CONFIG =====
 faker.seed(42);
@@ -78,91 +78,7 @@ async function seed() {
         ]);
     }
 
-    const defaultComponents = [
-        {
-            title: 'Total Pacientes',
-            type: 'KPI',
-            position: { x: 0, y: 0, w: 2, h: 2 },
-            config: {
-                visuals: { color: 'primary.main' },
-                query: { entity: 'Patient', aggregation: 'COUNT', targetColumn: 'id' },
-            },
-        },
-        {
-            title: 'Pacientes Activos',
-            type: 'KPI',
-            position: { x: 2, y: 0, w: 2, h: 2 },
-            config: {
-                visuals: { color: 'success.main' },
-                query: {
-                    entity: 'Patient',
-                    aggregation: 'COUNT',
-                    targetColumn: 'id',
-                    filters: { status: 'ACTIVE' },
-                },
-            },
-        },
-        {
-            title: 'Pacientes a lo largo del tiempo',
-            type: 'LINE_CHART',
-            position: { x: 0, y: 2, w: 4, h: 3 },
-            config: {
-                visuals: { xAxisKey: 'x', yAxisKey: 'y', yAxisLabel: 'Pacientes', tooltipLabel: 'Pacientes' },
-                query: {
-                    entity: 'Patient',
-                    aggregation: 'COUNT',
-                    targetColumn: 'id',
-                    groupBy: 'createdAt',
-                    timeGrain: 'week',
-                },
-            },
-        },
-        {
-            title: 'Citas a lo largo del tiempo',
-            type: 'LINE_CHART',
-            position: { x: 0, y: 5, w: 4, h: 3 },
-            config: {
-                visuals: { xAxisKey: 'x', yAxisKey: 'y', yAxisLabel: 'Citas', tooltipLabel: 'Citas' },
-                query: {
-                    entity: 'Appointment',
-                    aggregation: 'COUNT',
-                    targetColumn: 'id',
-                    groupBy: 'startTime',
-                    timeGrain: 'week',
-                },
-            },
-        },
-        {
-            title: 'Distribución por Severidad',
-            type: 'BAR_CHART',
-            position: { x: 4, y: 0, w: 2, h: 4 },
-            config: {
-                visuals: { xAxisKey: 'x', yAxisKey: 'y', tooltipLabel: 'Diagnósticos' },
-                query: {
-                    entity: 'Diagnosis',
-                    aggregation: 'COUNT',
-                    targetColumn: 'id',
-                    groupBy: 'severity',
-                    filters: { status: 'VALID' },
-                },
-            },
-        },
-        {
-            title: 'Estado de Tratamientos',
-            type: 'PIE_CHART',
-            position: { x: 4, y: 4, w: 2, h: 4 },
-            config: {
-                visuals: { nameKey: 'x', valueKey: 'y', tooltipLabel: 'Tratamientos' },
-                query: {
-                    entity: 'Treatment',
-                    aggregation: 'COUNT',
-                    targetColumn: 'id',
-                    groupBy: 'status',
-                    filters: { status: 'VALID' },
-                },
-            },
-        },
-    ];
+    const defaultComponents = DEFAULT_DASHBOARD_COMPONENTS; 
 
     const roles = await Role.findAll();
 
@@ -192,190 +108,190 @@ async function seed() {
         }))
     );
 
-    // ===== USERS =====
-    const users = [];
-    for (let i = 0; i < 25; i++) {
-        const user = await User.create({
-            name: faker.person.firstName(),
-            surname: faker.person.lastName(),
-            email: faker.internet.email() + i,
-            password,
-            phone: '6' + faker.string.numeric(8),
-            agendaId: agendas[i % agendas.length].id,
-            status: i % 6 === 0 ? 'INACTIVE' : 'ACTIVE',
-        });
+    // // ===== USERS =====
+    // const users = [];
+    // for (let i = 0; i < 25; i++) {
+    //     const user = await User.create({
+    //         name: faker.person.firstName(),
+    //         surname: faker.person.lastName(),
+    //         email: faker.internet.email() + i,
+    //         password,
+    //         phone: '6' + faker.string.numeric(8),
+    //         agendaId: agendas[i % agendas.length].id,
+    //         status: i % 6 === 0 ? 'INACTIVE' : 'ACTIVE',
+    //     });
 
-        users.push(user);
+    //     users.push(user);
 
-        // 20% admins, 80% cardiologists
-        const isAdmin = faker.number.int({ min: 1, max: 5 }) === 1;
-        user.addRole(isAdmin ? ADMIN_ROLE : CARDIO_ROLE);
+    //     // 20% admins, 80% cardiologists
+    //     const isAdmin = faker.number.int({ min: 1, max: 5 }) === 1;
+    //     user.addRole(isAdmin ? ADMIN_ROLE : CARDIO_ROLE);
 
-        const dashboard = await UserDashboard.create({
-            userId: user.id,
-        });
+    //     const dashboard = await UserDashboard.create({
+    //         userId: user.id,
+    //     });
 
-        await DashboardComponent.bulkCreate(
-            defaultComponents.map(component => ({
-                ...component,
-                dashboardId: dashboard.id,
-            }))
-        );
-    }
+    //     await DashboardComponent.bulkCreate(
+    //         defaultComponents.map(component => ({
+    //             ...component,
+    //             dashboardId: dashboard.id,
+    //         }))
+    //     );
+    // }
 
-    function generateDni(num) {
-        const letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
-        return `${String(num).padStart(8, '0')}${letters[num % 23]}`;
-    }
+    // function generateDni(num) {
+    //     const letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    //     return `${String(num).padStart(8, '0')}${letters[num % 23]}`;
+    // }
 
-    // ===== PATIENTS =====
-    const patients = [];
-    for (let i = 0; i < 100; i++) {
-        const createdAt = daysAgo(rand([5, 20, 60, 120]));
+    // // ===== PATIENTS =====
+    // const patients = [];
+    // for (let i = 0; i < 100; i++) {
+    //     const createdAt = daysAgo(rand([5, 20, 60, 120]));
 
-        const patient = await Patient.create({
-            name: faker.person.firstName(),
-            surname: faker.person.lastName(),
-            email: faker.internet.email() + i,
-            phone: '7' + faker.string.numeric(8),
-            addressLine1: faker.location.streetAddress(),
-            addressLine2: faker.datatype.boolean() ? faker.location.secondaryAddress() : null,
-            sex: rand(['MALE', 'FEMALE']),
-            dateOfBirth: faker.date.birthdate({ min: 18, max: 90, mode: 'age' }),
-            nhc: `${String(i).padStart(6, '0')}`,
-            dni: generateDni(47583940 + i),
-            status: i % 10 === 0 ? 'INACTIVE' : 'ACTIVE',
-            createdAt,
-            updatedAt: createdAt,
-        });
-        patients.push(patient);
-    }
+    //     const patient = await Patient.create({
+    //         name: faker.person.firstName(),
+    //         surname: faker.person.lastName(),
+    //         email: faker.internet.email() + i,
+    //         phone: '7' + faker.string.numeric(8),
+    //         addressLine1: faker.location.streetAddress(),
+    //         addressLine2: faker.datatype.boolean() ? faker.location.secondaryAddress() : null,
+    //         sex: rand(['MALE', 'FEMALE']),
+    //         dateOfBirth: faker.date.birthdate({ min: 18, max: 90, mode: 'age' }),
+    //         nhc: `${String(i).padStart(6, '0')}`,
+    //         dni: generateDni(47583940 + i),
+    //         status: i % 10 === 0 ? 'INACTIVE' : 'ACTIVE',
+    //         createdAt,
+    //         updatedAt: createdAt,
+    //     });
+    //     patients.push(patient);
+    // }
 
-    // ===== APPOINTMENTS =====
-    const appointments = [];
+    // // ===== APPOINTMENTS =====
+    // const appointments = [];
 
-    for (let i = 0; i < 200; i++) {
-        const patient = patients[i % patients.length];
-        const user = users[i % users.length];
+    // for (let i = 0; i < 200; i++) {
+    //     const patient = patients[i % patients.length];
+    //     const user = users[i % users.length];
 
-        let status,
-            startTime,
-            endTime = null;
+    //     let status,
+    //         startTime,
+    //         endTime = null;
 
-        if (i < 80) {
-            status = rand(['COMPLETED', 'NO_SHOW']);
-            startTime = daysAgo(rand([10, 30, 60]));
-            endTime = new Date(startTime.getTime() + 30 * 60000);
-        } else if (i < 120) {
-            status = 'CHECKED_IN';
-            startTime = daysAgo(1);
-        } else if (i < 170) {
-            status = 'SCHEDULED';
-            startTime = daysAhead(rand([1, 5, 10]));
-        } else {
-            status = 'CANCELLED';
-            startTime = daysAhead(rand([2, 6]));
-        }
+    //     if (i < 80) {
+    //         status = rand(['COMPLETED', 'NO_SHOW']);
+    //         startTime = daysAgo(rand([10, 30, 60]));
+    //         endTime = new Date(startTime.getTime() + 30 * 60000);
+    //     } else if (i < 120) {
+    //         status = 'CHECKED_IN';
+    //         startTime = daysAgo(1);
+    //     } else if (i < 170) {
+    //         status = 'SCHEDULED';
+    //         startTime = daysAhead(rand([1, 5, 10]));
+    //     } else {
+    //         status = 'CANCELLED';
+    //         startTime = daysAhead(rand([2, 6]));
+    //     }
 
-        appointments.push(
-            await Appointment.create({
-                userId: user.id,
-                patientId: patient.id,
-                reason: rand([
-                    'Consulta general',
-                    'Revisión',
-                    'Chequeo rutinario',
-                    'Resultados de laboratorio',
-                    'Consulta especializada',
-                ]),
-                location: faker.location.city(),
-                startTime,
-                endTime,
-                status,
-                type: rand(['IN_PERSON', 'VIRTUAL']),
-            })
-        );
-    }
+    //     appointments.push(
+    //         await Appointment.create({
+    //             userId: user.id,
+    //             patientId: patient.id,
+    //             reason: rand([
+    //                 'Consulta general',
+    //                 'Revisión',
+    //                 'Chequeo rutinario',
+    //                 'Resultados de laboratorio',
+    //                 'Consulta especializada',
+    //             ]),
+    //             location: faker.location.city(),
+    //             startTime,
+    //             endTime,
+    //             status,
+    //             type: rand(['IN_PERSON', 'VIRTUAL']),
+    //         })
+    //     );
+    // }
 
-    // ===== DIAGNOSES =====
-    const diagnoses = [];
+    // // ===== DIAGNOSES =====
+    // const diagnoses = [];
 
-    for (let i = 0; i < 120; i++) {
-        const patient = patients[i % patients.length];
-        const appointment = appointments[i % appointments.length];
+    // for (let i = 0; i < 120; i++) {
+    //     const patient = patients[i % patients.length];
+    //     const appointment = appointments[i % appointments.length];
 
-        const clinicalStatus = rand(['ACTIVE', 'RESOLVED', 'CHRONIC', 'RULED_OUT']);
-        const diagnosedAt = daysAgo(rand([5, 20, 60]));
+    //     const clinicalStatus = rand(['ACTIVE', 'RESOLVED', 'CHRONIC', 'RULED_OUT']);
+    //     const diagnosedAt = daysAgo(rand([5, 20, 60]));
 
-        const resolvedAt = clinicalStatus === 'RESOLVED' ? new Date(diagnosedAt.getTime() + 5 * 86400000) : null;
+    //     const resolvedAt = clinicalStatus === 'RESOLVED' ? new Date(diagnosedAt.getTime() + 5 * 86400000) : null;
 
-        const diagnosis = await Diagnosis.create({
-            patientId: patient.id,
-            appointmentId: appointment.id,
-            name: rand(['Hipertensión', 'Diabetes tipo II', 'Migraña', 'Ansiedad', 'Dolor lumbar']),
-            description: faker.lorem.sentence(),
-            notes: faker.datatype.boolean() ? faker.lorem.paragraph() : null,
-            severity: rand(['LOW', 'MODERATE', 'HIGH', 'CRITICAL']),
-            clinicalStatus,
-            status: rand(['VALID', 'VOID', 'ENTERED_IN_ERROR']),
-            diagnosedAt,
-            resolvedAt,
-        });
+    //     const diagnosis = await Diagnosis.create({
+    //         patientId: patient.id,
+    //         appointmentId: appointment.id,
+    //         name: rand(['Hipertensión', 'Diabetes tipo II', 'Migraña', 'Ansiedad', 'Dolor lumbar']),
+    //         description: faker.lorem.sentence(),
+    //         notes: faker.datatype.boolean() ? faker.lorem.paragraph() : null,
+    //         severity: rand(['LOW', 'MODERATE', 'HIGH', 'CRITICAL']),
+    //         clinicalStatus,
+    //         status: rand(['VALID', 'VOID', 'ENTERED_IN_ERROR']),
+    //         diagnosedAt,
+    //         resolvedAt,
+    //     });
 
-        diagnoses.push(diagnosis);
+    //     diagnoses.push(diagnosis);
 
-        await DiagnosisUser.create({
-            diagnosisId: diagnosis.id,
-            userId: users[i % users.length].id,
-            role: 'AUTHOR',
-        });
-    }
+    //     await DiagnosisUser.create({
+    //         diagnosisId: diagnosis.id,
+    //         userId: users[i % users.length].id,
+    //         role: 'AUTHOR',
+    //     });
+    // }
 
-    // ===== TREATMENTS =====
-    for (let i = 0; i < 120; i++) {
-        const patient = patients[i % patients.length];
-        const appointment = appointments[i % appointments.length];
+    // // ===== TREATMENTS =====
+    // for (let i = 0; i < 120; i++) {
+    //     const patient = patients[i % patients.length];
+    //     const appointment = appointments[i % appointments.length];
 
-        const clinicalStatus = rand(['PLANNED', 'ONGOING', 'GIVEN', 'COMPLETED', 'DISCONTINUED']);
-        const devisedAt = daysAgo(rand([5, 20, 60]));
+    //     const clinicalStatus = rand(['PLANNED', 'ONGOING', 'GIVEN', 'COMPLETED', 'DISCONTINUED']);
+    //     const devisedAt = daysAgo(rand([5, 20, 60]));
 
-        const resolvedAt = ['COMPLETED', 'DISCONTINUED'].includes(clinicalStatus)
-            ? new Date(devisedAt.getTime() + 5 * 86400000)
-            : null;
+    //     const resolvedAt = ['COMPLETED', 'DISCONTINUED'].includes(clinicalStatus)
+    //         ? new Date(devisedAt.getTime() + 5 * 86400000)
+    //         : null;
 
-        const treatment = await Treatment.create({
-            patientId: patient.id,
-            appointmentId: appointment.id,
+    //     const treatment = await Treatment.create({
+    //         patientId: patient.id,
+    //         appointmentId: appointment.id,
 
-            diagnosisId: i % 2 === 0 ? diagnoses[i % diagnoses.length].id : null,
+    //         diagnosisId: i % 2 === 0 ? diagnoses[i % diagnoses.length].id : null,
 
-            name: rand([
-                'Terapia farmacológica',
-                'Fisioterapia',
-                'Dieta controlada',
-                'Terapia cognitiva',
-                'Tratamiento del dolor',
-            ]),
+    //         name: rand([
+    //             'Terapia farmacológica',
+    //             'Fisioterapia',
+    //             'Dieta controlada',
+    //             'Terapia cognitiva',
+    //             'Tratamiento del dolor',
+    //         ]),
 
-            description: faker.lorem.sentence(),
+    //         description: faker.lorem.sentence(),
 
-            notes: faker.datatype.boolean() ? faker.lorem.paragraph() : null,
+    //         notes: faker.datatype.boolean() ? faker.lorem.paragraph() : null,
 
-            duration: rand(['1 semana', '2 semanas', '1 mes', '3 meses']),
+    //         duration: rand(['1 semana', '2 semanas', '1 mes', '3 meses']),
 
-            clinicalStatus,
-            status: rand(['VALID', 'VOID', 'ENTERED_IN_ERROR']),
+    //         clinicalStatus,
+    //         status: rand(['VALID', 'VOID', 'ENTERED_IN_ERROR']),
 
-            devisedAt,
-            resolvedAt,
-        });
+    //         devisedAt,
+    //         resolvedAt,
+    //     });
 
-        await TreatmentUser.create({
-            treatmentId: treatment.id,
-            userId: users[i % users.length].id,
-            role: 'AUTHOR',
-        });
-    }
+    //     await TreatmentUser.create({
+    //         treatmentId: treatment.id,
+    //         userId: users[i % users.length].id,
+    //         role: 'AUTHOR',
+    //     });
+    // }
 
     console.log('Full deterministic realistic seed completed');
 }
